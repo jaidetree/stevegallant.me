98d009c0c30790b1562f6d7e0384a77183a814f6
Jay Zawrotny <jayzawrotny@gmail.com>
2014-09-22 18:55:32 -0400

    Set scrol to window instead of html, body.

--- static/js/main.js
+++ static/js/main.js
@@ -426,7 +426,7 @@ App.views.LayoutManager = Backbone.View.extend({
 
     steveGallant.$el.find('#navbar li').removeClass('active');
     steveGallant.$el.find('#navbar li.' + name).addClass('active');
-    $('html, body').animate({
+    $(window).animate({
       scrollTop: 0
     }, 500);
 
